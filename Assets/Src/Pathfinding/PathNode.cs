9f6e6bf303c40840272aefbac19466d59a633cdd
mehrdad <mehrdad@artifact5.com>
2018-12-08 15:05:11 -0500

    updated player locomotion
    movement is now based on grid rather than collision detection

---
+++ Assets/Src/Pathfinding/PathNode.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PathNode : MonoBehaviour {
+
+    public PathNode[] neighbors;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(this.transform.position, 0.1f);
+        if(this.neighbors != null)
+            for (int i = 0; i < this.neighbors.Length; i++)
+                Gizmos.DrawLine(this.transform.position, this.neighbors[i].transform.position);
+    }
+
+    public PathNode Right() {
+        for(int i = 0; i < this.neighbors.Length; i++) {
+            if (this.neighbors[i].transform.position.x > this.transform.position.x)
+                return neighbors[i];
+        }
+        return null;
+    }
+
+    public PathNode Left() {
+        for (int i = 0; i < this.neighbors.Length; i++) {
+            if (this.neighbors[i].transform.position.x < this.transform.position.x)
+                return neighbors[i];
+        }
+        return null;
+    }
+
+    public PathNode Up() {
+        for (int i = 0; i < this.neighbors.Length; i++) {
+            if (this.neighbors[i].transform.position.y > this.transform.position.y)
+                return neighbors[i];
+        }
+        return null;
+    }
+
+    public PathNode Down() {
+        for (int i = 0; i < this.neighbors.Length; i++) {
+            if (this.neighbors[i].transform.position.y < this.transform.position.y)
+                return neighbors[i];
+        }
+        return null;
+    }
+}
--- Assets/Src/Player/Player.cs
+++ Assets/Src/Player/Player.cs
@@ -3,29 +3,43 @@
 public class Player : MonoBehaviour {
 
     public float speed;
+    public PathNode start;
 
     private GameInput gi;
-    private CharacterController cc;
+    private PathNode current;
 
     private void Awake() {
         this.gi = Toolbox.Instance.GetTool<GameInput>("GameInput");
-        this.cc = this.GetComponent<CharacterController>();
+        this.current = this.start;
     }
 
     private void FixedUpdate () {
         // aquire input directions from GameInput
         float xDir = ((this.gi.rightPressed) ? 1f : -1f) + ((this.gi.leftPressed) ? -1f : 1f);
         float yDir = ((this.gi.upPressed) ? 1f : -1f) + ((this.gi.downPressed) ? -1f : 1f);
-        if (xDir != 0) { // horizontal direction takes precedence over vertical
-            this.UpdateLocomotion(new Vector2(xDir, 0f));
-        } else if (yDir != 0) {
-            this.UpdateLocomotion(new Vector2(0f, yDir));
-        }
+        this.UpdateLocomotion(new Vector2(xDir, yDir));
     }
 
     private void UpdateLocomotion(Vector2 dir) {
-        // move the player based on given input and speed
-        this.cc.Move(this.speed * Time.fixedDeltaTime * new Vector3(dir.x, dir.y));
+        PathNode target = null;
+        // horizontal direction takes precedence over vertical
+        if (dir.x > 0) {
+            target = this.current.Right();
+        } else if (dir.x < 0) {
+            target = this.current.Left();
+        } else if (dir.y > 0) {
+            target = this.current.Up();
+        } else if (dir.y < 0) {
+            target = this.current.Down();
+        }
+        if(target) {
+            // move towards target and check if it is reached
+            Vector3 motion = (target.transform.position - this.transform.position);
+            this.transform.position += Time.fixedDeltaTime * motion.normalized;
+            if (motion.magnitude <= Mathf.Epsilon) {
+                this.current = target;
+            }
+        }
         // set the direction of player facing towards
         this.transform.right = new Vector3(this.transform.right.x + dir.x, this.transform.right.y + dir.y, this.transform.right.z).normalized;
     }
