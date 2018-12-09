b1fa16d2fa2e65eccc214f33cdbee9286da42322
mehrdad <mehrdad@artifact5.com>
2018-12-09 00:36:25 -0500

    Player locomotion complete
    Updated the locomotion to work with grid-based pathnodes
    Pacman level half-filled with pathnodes

--- Assets/Src/Pathfinding/PathNode.cs
+++ Assets/Src/Pathfinding/PathNode.cs
@@ -4,43 +4,62 @@ public class PathNode : MonoBehaviour {
 
     public PathNode[] neighbors;
 
+    public PathNode right {
+        get {
+            for (int i = 0; i < this.neighbors.Length; i++) {
+                if ((int)this.neighbors[i].transform.position.x > (int)this.transform.position.x)
+                    return neighbors[i];
+            }
+            return this;
+        }
+    }
+
+    public PathNode left {
+        get {
+            for (int i = 0; i < this.neighbors.Length; i++) {
+                if ((int)this.neighbors[i].transform.position.x < (int)this.transform.position.x)
+                    return neighbors[i];
+            }
+            return this;
+        }
+    }
+
+    public PathNode up {
+        get {
+            for (int i = 0; i < this.neighbors.Length; i++) {
+                if ((int)this.neighbors[i].transform.position.y > (int)this.transform.position.y)
+                    return neighbors[i];
+            }
+            return this;
+        }
+    }
+
+    public PathNode down {
+        get {
+            for (int i = 0; i < this.neighbors.Length; i++) {
+                if ((int)this.neighbors[i].transform.position.y < (int)this.transform.position.y) {
+                    return neighbors[i];
+                }
+            }
+            return this;
+        }
+    }
+
+#if UNITY_EDITOR
     private void OnDrawGizmos() {
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(this.transform.position, 0.1f);
-        if(this.neighbors != null)
+        if (this.neighbors != null)
             for (int i = 0; i < this.neighbors.Length; i++)
-                Gizmos.DrawLine(this.transform.position, this.neighbors[i].transform.position);
+                if(this.neighbors[i].IsNeighbor(this))
+                    Gizmos.DrawLine(this.transform.position, this.neighbors[i].transform.position);
     }
 
-    public PathNode Right() {
-        for(int i = 0; i < this.neighbors.Length; i++) {
-            if (this.neighbors[i].transform.position.x > this.transform.position.x)
-                return neighbors[i];
-        }
-        return null;
+    public bool IsNeighbor(PathNode pathNode) {
+        for (int i = 0; i < this.neighbors.Length; i++)
+            if (this.neighbors[i] == pathNode)
+                return true;
+        return false;
     }
-
-    public PathNode Left() {
-        for (int i = 0; i < this.neighbors.Length; i++) {
-            if (this.neighbors[i].transform.position.x < this.transform.position.x)
-                return neighbors[i];
-        }
-        return null;
-    }
-
-    public PathNode Up() {
-        for (int i = 0; i < this.neighbors.Length; i++) {
-            if (this.neighbors[i].transform.position.y > this.transform.position.y)
-                return neighbors[i];
-        }
-        return null;
-    }
-
-    public PathNode Down() {
-        for (int i = 0; i < this.neighbors.Length; i++) {
-            if (this.neighbors[i].transform.position.y < this.transform.position.y)
-                return neighbors[i];
-        }
-        return null;
-    }
+#endif
 }
--- Assets/Src/Player/Player.cs
+++ Assets/Src/Player/Player.cs
@@ -3,10 +3,14 @@
 public class Player : MonoBehaviour {
 
     public float speed;
+    public float minReachDistance;
     public PathNode start;
 
     private GameInput gi;
     private PathNode current;
+    private PathNode target;
+    private bool xMotion;
+    private bool yMotion;
 
     private void Awake() {
         this.gi = Toolbox.Instance.GetTool<GameInput>("GameInput");
@@ -21,26 +25,29 @@ public class Player : MonoBehaviour {
     }
 
     private void UpdateLocomotion(Vector2 dir) {
-        PathNode target = null;
         // horizontal direction takes precedence over vertical
-        if (dir.x > 0) {
-            target = this.current.Right();
-        } else if (dir.x < 0) {
-            target = this.current.Left();
-        } else if (dir.y > 0) {
-            target = this.current.Up();
-        } else if (dir.y < 0) {
-            target = this.current.Down();
+        if(dir.x != 0) {
+            if (this.yMotion) return;
+            this.xMotion = true;
+            this.target = (dir.x > 0) ? this.current.right : this.current.left;
+        } else if(dir.y != 0) {
+            if (this.xMotion) return;
+            this.yMotion = true;
+            this.target = (dir.y > 0) ? this.current.up : this.current.down;
         }
-        if(target) {
+        if(this.target && dir.magnitude != 0f) {
             // move towards target and check if it is reached
-            Vector3 motion = (target.transform.position - this.transform.position);
-            this.transform.position += Time.fixedDeltaTime * motion.normalized;
-            if (motion.magnitude <= Mathf.Epsilon) {
-                this.current = target;
+            Vector3 motion = (this.target.transform.position - this.transform.position);
+            if (motion.magnitude <= this.minReachDistance) {
+                this.current = this.target;
+                this.target = null;
             }
+            if ((this.transform.position - this.current.transform.position).magnitude <= this.minReachDistance) {
+                this.xMotion = this.yMotion = false;
+            }
+            this.transform.position += motion.normalized * this.speed * Time.fixedDeltaTime;
+            // set the direction of player facing towards
+            this.transform.right = new Vector3(this.transform.right.x + dir.x, this.transform.right.y + dir.y, this.transform.right.z).normalized;
         }
-        // set the direction of player facing towards
-        this.transform.right = new Vector3(this.transform.right.x + dir.x, this.transform.right.y + dir.y, this.transform.right.z).normalized;
     }
 }
